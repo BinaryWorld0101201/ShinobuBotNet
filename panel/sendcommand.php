128acfb0e2b4e5ea2121c4ba6a8151579b7372d3
Amadeus <58073614+martokolion@users.noreply.github.com>
2020-06-18 17:49:56 +0500

    Update sendcommand.php

--- panel/sendcommand.php
+++ panel/sendcommand.php
@@ -3,14 +3,30 @@ include 'class/sql.php';
 $id_user = $_GET['id'];
 $type = $_GET['type'];
 $content = $_GET['content'];
-$sql = 'INSERT INTO command SET id_user = "'.$id_user.'", commandType = "'.$type.'", commandContent="'.$content.'"';
-$result = mysqli_query($link, $sql);
-
-if ($result == True) {
-    print("command send!");
+if($content == null)
+{
+    $sql = 'INSERT INTO command SET id_user = "'.$id_user.'", commandType = "'.$type.'"';
+    $result = mysqli_query($link, $sql);
+    if ($result == True) {
+        print("command send!");
+    }
+    else
+    {
+        print("error");
+    }
 }
 else
 {
-    print("error");
+    $sql = 'INSERT INTO command SET id_user = "'.$id_user.'", commandType = "'.$type.'", commandContent="'.$content.'"';
+    $result = mysqli_query($link, $sql);
+
+    if ($result == True) {
+        print("command send!");
+    }
+    else
+    {
+        print("error");
+    }
 }
+
 ?>
