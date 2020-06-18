1af871b639c52f7b42a9f5183e8435128739302d
Amadeus <58073614+martokolion@users.noreply.github.com>
2020-06-18 17:31:31 +0500

    update GetUsers.php and create client

--- panel/getusers.php
+++ panel/getusers.php
@@ -5,7 +5,7 @@ $sql = 'SELECT * FROM `users`';
 $result = mysqli_query($link, $sql);
 
 while ($row = mysqli_fetch_array($result)) {
-    print ('"id": "'.$row['id'].'"')
+    print ('"user": "'.$row['id'].'"')
 }
 
 ?>
