018a2eaef77f3c3741e57a9b69e8acb5c5ec6a99
Amadeus <58073614+Amadeus452@users.noreply.github.com>
2020-06-22 20:18:37 +0500

    Update getusers.php

--- panel/getusers.php
+++ panel/getusers.php
@@ -5,7 +5,7 @@ $sql = 'SELECT * FROM `users`';
 $result = mysqli_query($link, $sql);
 
 while ($row = mysqli_fetch_array($result)) {
-    print ('"user": "Id: '.$row['id'].' Ip: '.$row['ip'].' UserName: '.$row['PC_user'].'"')
+    echo 'ID: '.$row['id'].' , IP: '.$row['ip'].' , User Name: '.$row['PC_user'];
 }
 
 ?>
