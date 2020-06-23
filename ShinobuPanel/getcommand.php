949ff24a004d8b182c443940125bafb87780c344
Amadeus <58073614+Amadeus452@users.noreply.github.com>
2020-06-23 21:28:33 +0500

    small fix

--- ShinobuPanel/getcommand.php
+++ ShinobuPanel/getcommand.php
@@ -5,14 +5,9 @@ $id = $_GET['id'];
 $sql = 'SELECT * FROM command WHERE id_user="'.$id.'"';
 
 $result = mysqli_query($link, $sql);
-if($result == true)
-{
-	while ($row = mysqli_fetch_array($result)) {
-    print($row['commandType'].'{split}'.$row['commandContent']);
+while ($row = mysqli_fetch_array($result)) {
+	print($row['commandType'].'{split}'.$row['commandContent']);
 }
-}
-else
-{
-	print('{split}')
-}
+
+
 ?>
--- ShinobuPanel/sendcommand.php
+++ ShinobuPanel/sendcommand.php
@@ -5,18 +5,43 @@ $type = $_GET['type'];
 $content = $_GET['content'];
 if($content == null)
 {
-    $sql = 'INSERT INTO command SET id_user = "'.$id_user.'", commandType = "'.$type.'"';
-    $result = mysqli_query($link, $sql);
-    if ($result == True) {
-        print("command send!");
+    $deletecommand = 'DELETE FROM command WHERE id_user = "'.$id_user.'"';
+    $deletresult = mysqli_query($link, $deletecommand);
+
+    if($deletresult == true)
+    {
+        echo 'old command delet'.'<br/>';
+        $sql = 'INSERT INTO `command`(`id_user`, `commandType`, `commandContent`) VALUES ("'.$id_user.'","'.$type.'","")';
+        $result = mysqli_query($link, $sql);
+
+        if ($result == true) {
+            echo 'command send!'.'<br/>';
+        }
+
+        else
+        {
+            echo 'ERROR!'.'<br/>';
+        }
     }
+
     else
     {
-        print("error");
+        echo 'old command not delete!'.'<br/>';
+        echo 'ERROR!';
     }
+
 }
+
 else
 {
+    $deletecommand = 'DELETE FROM command WHERE id_user = "'.$id_user.'"';
+    $deletresult = mysqli_query($link, $deletecommand);
+    if($deletresult == true)
+    {
+        echo 'old command delet';
+    }
+    
+
     $sql = 'INSERT INTO command SET id_user = "'.$id_user.'", commandType = "'.$type.'", commandContent="'.$content.'"';
     $result = mysqli_query($link, $sql);
 
