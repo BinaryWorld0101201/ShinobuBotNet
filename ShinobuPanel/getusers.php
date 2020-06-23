a7d5d16709b33daf51102b212708da4c84362f47
Amadeus <58073614+Amadeus452@users.noreply.github.com>
2020-06-23 14:36:50 +0500

    Update getusers.php

--- ShinobuPanel/getusers.php
+++ ShinobuPanel/getusers.php
@@ -5,7 +5,7 @@ $sql = 'SELECT * FROM `users`';
 $result = mysqli_query($link, $sql);
 
 while ($row = mysqli_fetch_array($result)) {
-    echo 'ID: '.$row['id'].' , IP: '.$row['ip'].' , User Name: '.$row['PC_user'];
+    echo 'ID: '.$row['id'].' , IP: '.$row['ip'].' , User Name: '.$row['PC_user'].'<br/>';
 }
 
 ?>
