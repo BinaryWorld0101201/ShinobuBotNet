f0710dd913bafcd3b43ebbbd87ac709392c0b36c
Amadeus <58073614+Amadeus452@users.noreply.github.com>
2020-06-25 15:54:15 +0500

    add files

--- ShinobuClient/Shinobu.py
+++ ShinobuClient/Shinobu.py
@@ -1,8 +1,10 @@
 import requests
 import os
-import json
 from colorama import Fore, Back, init
+import platform
 init(autoreset=True)
+
+
 print(Fore.MAGENTA + '''
 ░██████╗██╗░░██╗██╗███╗░░██╗░█████╗░██████╗░██╗░░░██╗██████╗░░█████╗░████████╗███╗░░██╗███████╗████████╗
 ██╔════╝██║░░██║██║████╗░██║██╔══██╗██╔══██╗██║░░░██║██╔══██╗██╔══██╗╚══██╔══╝████╗░██║██╔════╝╚══██╔══╝
@@ -13,18 +15,39 @@ print(Fore.MAGENTA + '''
 ''')
 
 if(os.path.isfile('URL.txt') == False):
-	file = open('URL.txt', 'w')
-	URL = input(Fore.YELLOW + '[?] - Enter URL: ')
-	file.write(URL)
-	file.close()
-	print(Fore.GREEN + '[!] - url ' + URL + ' set')
+	#installion
+	installion = input(Fore.YELLOW + 'Start installing the panel? (y/n): ')
+	#USER AGREEMENT
+	if (installion == "y"):
+		print('USER AGREEMENT: ')
+		print("I, the creator and all those associated with the development and production of this program are not responsible for any actions and or damages caused by this software. You bear the full responsibility of your actions and acknowledge that this software was created for educational purposes only. This software s intended purpose is NOT to be used maliciously, or on any system that you do not have own or have explicit permission to operate and use this program on. By using this software, you automatically agree to the above.")
+		a = input(Fore.YELLOW + 'Do you accept this agreement? (y/n): ')
+		if (a == 'y'):
+			os.system('clear')
+			URL = input(Fore.YELLOW + '[?] - Enter URL: ')
+			print(requests.get(URL + 'install.php?ans=Yes'))
+			print(Fore.GREEN + 'Installed')
+			#connect
+			file = open('URL.txt', 'w')
+			file.write(URL)
+			file.close()
+			print(Fore.GREEN + '[!] - url ' + URL + ' set')
+			exit()
+		else:
+			exit()
+	else:
+    	#connect
+		URL = input(Fore.YELLOW + '[?] - Enter URL: ')
+		file = open('URL.txt', 'w')
+		file.write(URL)
+		file.close()
+		print(Fore.GREEN + '[!] - url ' + URL + ' set')
+		exit()
 
-print(Fore.GREEN + '[!] - Get URL')
-file = open('URL.txt')
-url = file.read()
-print(Fore.GREEN + '[!] - URL set')
-response = requests.get(url + 'GetUsers.php')
-userslist = json.load(response.text)
-
-for user in userslist():
-	print(user['user'])
+else:
+	print(Fore.GREEN + '[!] - Get URL')
+	file = open('URL.txt')
+	url = file.read()
+	print(Fore.GREEN + '[!] - URL set ' + url)
+	userslist = requests.get(url + 'getusers.php')
+	print(userslist)
